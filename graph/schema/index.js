f21c28604d298f2f11a6239b71b817c77778961d
Sam Harris <sam@jam-bc.com>
2020-06-04 13:55:57 +0100

    created episode schema and model

--- graph/schema/index.js
+++ graph/schema/index.js
@@ -2,8 +2,9 @@ import core from './core';
 import queries from './_queries.js';
 import mutations from './_mutations.js';
 
-import user from './user.js';
+import episode from './episode';
 import friendship from './friendship';
 import podcast from './podcast';
+import user from './user.js';
 
-export default [core, user, friendship, podcast, queries, mutations];
+export default [core, episode, friendship, mutations, podcast, queries, user];
