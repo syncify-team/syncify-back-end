71ef7fefd5e8364138b26fe20c53e3c1964530cd
JedraPeake <jpeake2@uwo.ca>
2020-06-04 21:21:43 -0400

    changes to user

--- graph/resolvers/user.js
+++ graph/resolvers/user.js
@@ -1,51 +1,38 @@
-import User from '../../models/user'
+import knex from '../../config/knex'
 
 export default {
-	users: (_, params, context) => {
-		return User.fetchAll()
-			.then((users) => {
-				const retLists = [];
-				users.forEach((user) => {
-					retLists.push(user.attributes)
-				})
-				return retLists
-			})
-	},
+  users: (_, params, context) => {
+    return knex.from('users').select('*').then((users) => users)
+  },
 
-	user: (_, { id }) => {
-		return User.where({ id: id }).fetch().then((user) => user.attributes)
-	},
+  user: (_, { id }) => {
+    return knex.from('users').select('*').where({ id }).first().then((user) => user)
+  },
 
-	userByAuthId: (_, { auth0_id }) => {
-		return User.where({ auth0_id: auth0_id }).fetch().then((user) => user.attributes)
-	},
+  userByAuthId: (_, { auth0_id }) => {
+    return knex.from('users').select('*').where({ auth0_id }).first().then((user) => user)
+  },
 }
 
 const valid = (newUser) => {
-	if (newUser.username && newUser.email && newUser.first_name && newUser.last_name
-		&& newUser.social_login_type && newUser.auth0_id) {
-		return Promise.resolve(newUser)
-	} else {
-		return Promise.reject('Missing Parameters')
-	}
+  if (newUser.username && newUser.email && newUser.first_name && newUser.last_name
+    && newUser.social_login_type && newUser.auth0_id) {
+    return Promise.resolve(newUser)
+  } else {
+    return Promise.reject('Missing Parameters')
+  }
 }
 
-export const createUser = async (_, { input }) => {
-	await valid(input);
-
-	let returnObject = {};
-
-	let userPromise = await User.forge({
-		username: input.username,
-		email: input.email,
-		first_name: input.first_name,
-		last_name: input.last_name,
-		social_login_type: input.social_login_type,
-		auth0_id: input.auth0_id
-	}).save().then((user) => {
-		return returnObject = user.attributes
-	});
-
-	await userPromise;
-	return returnObject;
+export const createUser = (_, { input }) => {
+  return valid(input)
+    .then(() =>
+      knex('users').insert({
+        username: input.username,
+        email: input.email,
+        first_name: input.first_name,
+        last_name: input.last_name,
+        social_login_type: input.social_login_type,
+        auth0_id: input.auth0_id
+      }).returning('*').then((user) => user[0])
+    )
 }
