beb34562f83a20a3f1f200800e6ea33c13ed663e
Ali Ahsan <aliahsan07@outlook.com>
2020-06-06 14:11:31 -0500

    refactored auth and graphql config codebase

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-const { graphqlExpress } = require('apollo-server-express');
 import logger from 'morgan';
-import cookieParser from 'cookie-parser';
-import path from 'path';
-import session from 'express-session';
 import dotenv from 'dotenv';
-import passport from 'passport';
-import Auth0Strategy from 'passport-auth0';
 var jwks = require('jwks-rsa');
 const jwt = require('express-jwt');
 
 import graph, { graphUi } from './config/graphql';
-import Routes from './config/routes';
-import authRouter from './config/auth';
 
 const Bundler = require('parcel-bundler');
 let bundler = new Bundler('./server.js', { target: 'node' });
@@ -35,78 +27,14 @@ const auth = jwt({
     jwksRequestsPerMinute: 5,
     jwksUri: `${process.env.AUTH0_ISSUER}.well-known/jwks.json`,
   }),
-  audience: process.env.AUTH0_AUDIENCE,
+  aud: process.env.AUTH0_AUDIENCE,
   issuer: process.env.AUTH0_ISSUER,
   algorithms: ['RS256'],
-}).unless({
-  path: ['/login'],
 });
 
-// var sess = {
-//   secret: process.env.SESSION_SECRET,
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-// };
-
-if (process.env.NODE_ENV === 'production') {
-  // Use secure cookies in production (requires SSL/TLS)
-  sess.cookie.secure = true;
-}
-
-// var strategy = new Auth0Strategy(
-//   {
-//     domain: process.env.AUTH0_DOMAIN,
-//     clientID: process.env.AUTH0_CLIENT_ID,
-//     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-//     callbackURL: process.env.AUTH0_CALLBACK_URL,
-//   },
-//   function (accessToken, refreshToken, extraParams, profile, done) {
-//     console.log(extraParams.id_token);
-//     return done(null, profile);
-//   },
-// );
-
-// passport.use(strategy);
-// app.use(session(sess));
-// // need to be after app.use(session(sess)
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// passport.serializeUser((user, done) => {
-//   done(null, user);
-// });
-
-// passport.deserializeUser((user, done) => {
-//   done(null, user);
-// });
-
-// app.use((req, res, next) => {
-//   res.locals.isAuthenticated = req.isAuthenticated();
-//   next();
-// });
-
-// app.use('/', authRouter);
-
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
 app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-
-// GraphQL.configure(app);
-// Routes.configure(app);
-
-// app.listen(port, () => {
-//   console.log(`Syncify Server listening on port ${port}!`.bold);
-//   console.log(`-- GraphQL server started`.green);
-// });
-
 // graphql endpoint
-// app.use('/graphql', auth, bodyParser.json(), graph());
-app.use('/graphql', bodyParser.json(), graph());
+app.use('/graphql', auth, bodyParser.json(), graph());
 app.use('/graphiql', graphUi());
 
 app.listen(port, () => {
--- config/graphql.js
+++ config/graphql.js
@@ -1,12 +1,7 @@
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
-import Jwt from 'express-jwt';
-import jwksRsa from 'jwks-rsa';
-import Cors from 'cors';
-import colors from 'colors';
 import schema from '../graph/schema';
 import resolvers from '../graph/resolvers';
-import Connector from '../graph/connector';
 
 const graph = () => {
   const graphSchema = makeExecutableSchema({
@@ -15,20 +10,6 @@ const graph = () => {
   });
 
   return graphqlExpress((request) => {
-    // const user = request.user;
-
-    // const ip =
-    //   request.headers['x-forwarded-for'] ||
-    //   request.connection.remoteAddress ||
-    //   request.socket.remoteAddress ||
-    //   request.connection.socket.remoteAddress;
-
-    // const connector = new Connector({
-    //   uid: usePassport ? user.user_id : user.sub,
-    //   roles: ['user'],
-    //   ip: ip,
-    // });
-
     return {
       schema: graphSchema,
       context: {
@@ -44,55 +25,4 @@ export const graphUi = () => {
   });
 };
 
-// const cors = () => {
-//   return Cors({
-//     methods: ['POST'],
-//     allowedHeaders: 'Origin, Accept, Content-Type, Authorization',
-//   });
-// };
-
-// const jwt = () => {
-//   const secret = jwksRsa.expressJwtSecret({
-//     cache: true,
-//     rateLimit: true,
-//     jwksRequestsPerMinute: 5,
-//     jwksUri: `${process.env.AUTH0_ISSUER}.well-known/jwks.json`,
-//   });
-
-//   return Jwt({
-//     secret: secret,
-//     audience: process.env.AUTH0_AUDIENCE,
-//     issuer: process.env.AUTH0_ISSUER,
-//     algorithms: ['RS256'],
-//     getToken: (request) => {
-//       if (request.headers.authorization) {
-//         const [bearer, token] = request.headers.authorization.split(' ');
-//         return bearer === 'Bearer' && token ? token : null;
-//       }
-
-//       return null;
-//     },
-//   }).unless({ path: ['/admin/graphiql'] });
-// };
-
 export default graph;
-// export default class GraphQL {
-//   static configure(app) {
-//     app.use('/graphql', cors(), jwt(), graph());
-
-//     app.use(
-//       '/graphql-passport',
-//       cors(),
-//       (req, res, next) => {
-//         if (req.user) {
-//           return next();
-//         }
-//         req.session.returnTo = req.originalUrl;
-//         res.redirect('/login');
-//       },
-//       graph(true),
-//     );
-
-//     app.use('/graphiql', graphUi());
-//   }
-// }
