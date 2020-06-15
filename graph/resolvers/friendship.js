b9e22f2b12c47fac4b7e244cd6718afef0195708
samjam48 <s.w.h@live.co.uk>
2020-06-15 15:06:20 +0100

    fix knex change

--- graph/resolvers/friendship.js
+++ graph/resolvers/friendship.js
@@ -1,48 +1,5 @@
 import knex from '../../config/knex';
 
-<<<<<<< HEAD
-const friendships = () => knex.select('*').from('friendships')
-  .then((allFriendships) => allFriendships);
-
-const friendList = (_, { id }) => knex.from('friendships').where('user1_id', id).orWhere('user2_id', id)
-  .join('users AS a', 'a.id', 'friendships.user1_id')
-  .join('users AS b', 'b.id', 'friendships.user2_id')
-  .select(
-    'a.id as user1_id', 'a.username as user1_username', 'a.email as user1_email',
-    'a.first_name as user1_first_name', 'a.last_name as user1_last_name',
-
-    'b.id as user2_id', 'b.username as user2_username', 'b.email as user2_email',
-    'b.first_name as user2_first_name', 'b.last_name as user2_last_name',
-  )
-  .then((userFriendships) => {
-    const userFriendDetails = [];
-    userFriendships.map((friend) => {
-      if (friend.user1_id.toString() !== id.toString()) {
-        userFriendDetails.push({
-          friend: {
-            id: friend.user1_id,
-            username: friend.user1_username,
-            email: friend.user1_email,
-            first_name: friend.user1_first_name,
-            last_name: friend.user1_last_name,
-          },
-        });
-      } else if (friend.user2_id.toString() !== id.toString()) {
-        userFriendDetails.push({
-          friend: {
-            id: friend.user2_id,
-            username: friend.user2_username,
-            email: friend.user2_email,
-            first_name: friend.user2_first_name,
-            last_name: friend.user2_last_name,
-          },
-        });
-      }
-    });
-
-    return userFriendDetails;
-  });
-=======
 const friendships = (params, context) => {
   return knex.select('*').from('friendships')
     .then((friendships) => friendships)
@@ -55,11 +12,9 @@ const friendList = (_, { id }) => {
     .select(
       'a.id as user1_id', 'a.username as user1_username', 'a.email as user1_email',
       'a.first_name as user1_first_name', 'a.last_name as user1_last_name',
-      'a.social_login_type as user1_social_login_type', 'a.auth0_id as user1_auth0_id',
 
       'b.id as user2_id', 'b.username as user2_username', 'b.email as user2_email',
       'b.first_name as user2_first_name', 'b.last_name as user2_last_name',
-      'b.social_login_type as user2_social_login_type', 'b.auth0_id as user2_auth0_id',
     )
     .then((friendships) => {
       const friend_list = []
@@ -72,8 +27,6 @@ const friendList = (_, { id }) => {
                 email: friend.user1_email,
                 first_name: friend.user1_first_name,
                 last_name: friend.user1_last_name,
-                social_login_type: friend.user1_social_login_type,
-                auth0_id: friend.user1_auth0_id
               }
             })
         }
@@ -85,8 +38,6 @@ const friendList = (_, { id }) => {
                 email: friend.user2_email,
                 first_name: friend.user2_first_name,
                 last_name: friend.user2_last_name,
-                social_login_type: friend.user2_social_login_type,
-                auth0_id: friend.user2_auth0_id
               }
             })
         }
@@ -95,7 +46,6 @@ const friendList = (_, { id }) => {
       return friend_list
     })
 }
->>>>>>> development
 
 const valid = (newFriendship) => {
   if (
@@ -105,19 +55,6 @@ const valid = (newFriendship) => {
   ) {
     return Promise.resolve(newFriendship)
   }
-<<<<<<< HEAD
-  return Promise.reject(new Error('Missing Parameters'));
-};
-
-export const createFriendship = async (_, { input }) => valid(input)
-  .then(() => knex('friendships').insert({
-    user1_id: input.user1_id,
-    user2_id: input.user2_id,
-  }).returning('*').then((friendship) => friendship[0]));
-
-export const deleteFriendship = async (_, { id }) => knex('friendships')
-  .where({ id }).del().then((result) => result);
-=======
   return Promise.reject('Missing Parameters')
 }
 
@@ -134,6 +71,5 @@ export const createFriendship = async (_, { input }) => {
 export const deleteFriendship = async (_, { id }) => {
   return knex('friendships').where({ id }).del().then((result) => result)
 }
->>>>>>> development
 
 export default { friendships, friendList }
