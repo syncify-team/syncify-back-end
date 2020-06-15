3837cb83c2f99d07ff8b2dc0071ab006151250cd
samjam48 <s.w.h@live.co.uk>
2020-06-15 21:50:22 +0100

    duplicate friendships blocked, users now follow instead of freind

--- graph/resolvers/friendship.js
+++ graph/resolvers/friendship.js
@@ -6,6 +6,74 @@ const friendships = (params, context) => {
 }
 
 const friendList = (_, { id }) => {
+  return knex.from('friendships').where('user1_id', id)
+    .join('users AS b', 'b.id', 'friendships.user2_id')
+    .select(
+      'b.id as user2_id', 'b.username as user2_username', 'b.email as user2_email',
+      'b.first_name as user2_first_name', 'b.last_name as user2_last_name',
+      'b.image_url as user2_image_url',
+    )
+    .then((friendships) => {
+      const friend_list = []
+      friendships.map((friend) => {
+        if (friend.user2_id.toString() !== id.toString()) {
+          friend_list.push({
+              friend: {
+                id: friend.user2_id,
+                username: friend.user2_username,
+                email: friend.user2_email,
+                first_name: friend.user2_first_name,
+                last_name: friend.user2_last_name,
+                image_url: friend.user2_image_url
+              }
+            })
+        }
+      })
+
+      return friend_list
+    })
+}
+
+const valid = (newFriendship) => {
+  if (
+    newFriendship.user1_id
+    && newFriendship.user2_id
+    && newFriendship.user1_id !== newFriendship.user2_id
+  ) {
+    return Promise.resolve(newFriendship)
+  }
+  return Promise.reject('Missing Parameters')
+}
+
+export const createFriendship = async (_, { input }) => {
+  return valid(input)
+    .then(() =>
+      knex('friendships')
+      .where('user1_id', input.user1_id).andWhere('user2_id', input.user2_id)
+      .then(function(rows) {
+        if (rows.length===0) {
+          // no matching records found
+          return knex('friendships').insert({
+              user1_id: input.user1_id,
+              user2_id: input.user2_id,
+            }).returning('*').then((friendship) => friendship[0])
+        } else {
+          throw 'friendship connection already exists'
+        }
+      })
+    )
+}
+
+export const deleteFriendship = async (_, { id }) => {
+  return knex('friendships').where({ id }).del().then((result) => result)
+}
+
+export default { friendships, friendList }
+
+
+
+/* // --- Old friendList to work in both directions ---
+const friendList = (_, { id }) => {
   return knex.from('friendships').where('user1_id', id).orWhere('user2_id', id)
     .join('users AS a', 'a.id', 'friendships.user1_id')
     .join('users AS b', 'b.id', 'friendships.user2_id')
@@ -50,30 +118,4 @@ const friendList = (_, { id }) => {
       return friend_list
     })
 }
-
-const valid = (newFriendship) => {
-  if (
-    newFriendship.user1_id
-    && newFriendship.user2_id
-    && newFriendship.user1_id !== newFriendship.user2_id
-  ) {
-    return Promise.resolve(newFriendship)
-  }
-  return Promise.reject('Missing Parameters')
-}
-
-export const createFriendship = async (_, { input }) => {
-  return valid(input)
-    .then(() =>
-      knex('friendships').insert({
-        user1_id: input.user1_id,
-        user2_id: input.user2_id,
-      }).returning('*').then((friendship) => friendship[0])
-    )
-}
-
-export const deleteFriendship = async (_, { id }) => {
-  return knex('friendships').where({ id }).del().then((result) => result)
-}
-
-export default { friendships, friendList }
+*/
