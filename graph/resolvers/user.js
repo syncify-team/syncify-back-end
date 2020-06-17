a582a2e901449f05d1141956639dccc877b1b44d
samjam48 <s.w.h@live.co.uk>
2020-06-17 20:41:33 +0100

    find all results who are not user or followed by user

--- graph/resolvers/user.js
+++ graph/resolvers/user.js
@@ -32,12 +32,14 @@ const findUsersByInput = (_, { id, searchTerm }) => {
   return knex
     .select('*')
     .from('users AS a')
+    .join('friendships as b', 'b.user2_id', '=', 'a.id')
     .where( 'a.first_name', 'ilike', `%${searchTerm}%` )
     .orWhere( 'a.last_name', 'ilike', `%${searchTerm}%` )
     .orWhere( 'a.username', 'ilike', `%${searchTerm}%` )
     .orWhere( 'a.email', 'ilike', `%${searchTerm}%` )
-    .join('friendships as b','b.user2_id', '=', 'a.id')
-    // .select('*')
+    .whereNot(function() {
+      this.where('a.id', id).orWhere('b.user1_id', '=', id)
+    })
     .then((users) =>  {
       console.log('logging==========')
       console.log(users)
