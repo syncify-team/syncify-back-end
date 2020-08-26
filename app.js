734d85780bb22fc531fdfd511e2698cdffa14f41
samjam48 <s.w.h@live.co.uk>
2020-08-26 18:10:43 +0100

    add firestore config json to .env

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ import logger from 'morgan'
 import dotenv from 'dotenv'
 import graph, { graphUi } from './config/graphql'
 
-
 import * as admin from 'firebase-admin'
-const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS
+
+const serviceAccount = JSON.parse(`{
+  "type": "${process.env.type}",
+  "project_id": "${process.env.project_id}",
+  "private_key_id": "${process.env.private_key_id}",
+  "private_key": "${process.env.private_key}",
+  "client_email": "${process.env.client_email}",
+  "client_id": "${process.env.client_id}",
+  "auth_uri": "${process.env.auth_uri}",
+  "token_uri": "${process.env.token_uri}",
+  "auth_provider_x509_cert_url": "${process.env.auth_provider_x509_cert_url}",
+  "client_x509_cert_url": "${process.env.client_x509_cert_url}"
+}`)
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
