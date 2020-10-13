8a2848c6fb5c2542cae0bdcc00e6c81fe7049e7a
Ruiyang Zhang <ruiyang.zhang@fundingsocieties.com>
2020-10-13 17:40:41 +0800

    remove filters

--- graph/clients/listenNoteClient.js
+++ graph/clients/listenNoteClient.js
@@ -23,8 +23,9 @@ export const getEpisode = async (episode_id)=>{
 export const search = async (query, sort_by_date=0, type='podcast', offset=0, len_min=10, len_max=100,
   genre_ids=[68, 82], published_before=Date.now(), published_after=0,
   only_in=['title', 'description'], language='English', safe_mode=1)=>{
+  // return callListenNote(`/search?q=${query}&sort_by_date=${sort_by_date}&type=${type}&offset=${offset}&len_min=${len_min}&len_max=${len_max}&genre_ids=${genre_ids.join('%2C')}&published_before=${published_before}&published_after=${published_after}&only_in=${only_in.join('%2C')}&language=${language}&safe_mode=${safe_mode}`)
   //eslint-disable-next-line max-len
-  return callListenNote(`/search?q=${query}&sort_by_date=${sort_by_date}&type=${type}&offset=${offset}&len_min=${len_min}&len_max=${len_max}&genre_ids=${genre_ids.join('%2C')}&published_before=${published_before}&published_after=${published_after}&only_in=${only_in.join('%2C')}&language=${language}&safe_mode=${safe_mode}`)
+  return callListenNote(`/search?q=${query}&sort_by_date=${sort_by_date}&type=${type}&offset=${offset}&only_in=${only_in.join('%2C')}`)
 }
 
 
